6ff2b70cf60b37cf04d087fb5e43a8087f2d10de
Garrett Julaka <gnjulaka@gmail.com>
2021-11-15 21:04:15 -0600

    Iintial backend setup."

--- app/controllers/api/primitives_controller.rb
+++ app/controllers/api/primitives_controller.rb
@@ -1,9 +1,19 @@
 class Api::PrimitivesController < ApplicationController
 
     def index
+        @primitives = Primitive.all
+        render :index
     end
 
     def show
+        @primitive = Primitive.find(params[:id])
+        render :show
+    end
+
+    private
+
+    def primitive_params
+        params.require(:primitive).permit(:meaning, :stroke_count)
     end
 
 end
--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -1,9 +1,19 @@
 class Api::StoriesController < ApplicationController
 
     def index
+        @stories = Stories.all
+        render :index
     end
 
     def show
+        @story = Story.find(params[:id])
+        render :show
+    end
+
+    private
+
+    def story_params
+        params.require(:story).permit(:body, :kanji_id)
     end
 
 end
