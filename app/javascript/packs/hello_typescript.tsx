54404deb04e6db5dbde3666d035aa2f11953e4ec
Garrett Julaka <gnjulaka@gmail.com>
2021-11-23 23:40:28 -0600

    Typescript coming in OK.

--- app/javascript/packs/hello_typescript.tsx
+++ app/javascript/packs/hello_typescript.tsx
@@ -1,7 +1,8 @@
 // Run this example by adding <%= javascript_pack_tag 'hello_typescript' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb.
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react"
+import * as ReactDOM from "react-dom";
+import { Root } from '../components/root'
 
 interface HelloProps {
   name: string;
@@ -16,8 +17,11 @@ Hello.defaultProps = {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement("div")),
-  )
+  // ReactDOM.render(
+  //   <Hello name="React" />,
+  //   document.body.appendChild(document.createElement("div")),
+  // )
+
+  const root = document.getElementById("root")
+  ReactDOM.render( <Root />, root)
 })
